5eac7502eeba91a11443c78afdaf31540068dd50
Justin S. Leitgeb <jsl@hcoop.net>
2009-02-01 11:07:07 -0500

    Add support for compression of archived attributes

--- lib/archived_attributes/archived_attribute.rb
+++ lib/archived_attributes/archived_attribute.rb
@@ -1,31 +1,35 @@
 require 'activesupport'
 
-require File.expand_path(File.join(File.dirname(__FILE__),  %w[ backends filesystem ]))
+require File.expand_path(File.join(File.dirname(__FILE__),  
+    %w[ backends filesystem ]))
+
 require File.expand_path(File.join(File.dirname(__FILE__),  %w[ backends s3 ]))
 
 module ArchivedAttributes
   
   class ArchivedAttribute
-    attr_reader :name, :instance, :options
+    attr_reader :name, :instance
 
-    def initialize(name, instance, options = {})
+    def initialize(name, instance, opts = {})
       @name     = name
       @instance = instance
       @dirty    = false
 
-      @options = ArchivedAttributes::GlobalConfiguration.instance.to_hash.merge(options)
+      @_options = ArchivedAttributes::GlobalConfiguration.instance.to_hash.
+        merge(opts)
 
-      @backend = instantiate_backend_from(@options)
+      @backend = instantiate_backend_from(options)
     end
 
     def value
       val = defined?(@stashed_value) ? @stashed_value : @backend.load
-      marshal? ? Marshal.load(val) : val
+      val = gzip? ? Zlib::Inflate.inflate(val) : val
+      val = marshal? ? Marshal.load(val) : val
     end
 
     def value=(other)
       other = marshal? ? Marshal.dump(other) : other
-
+      other = gzip? ? Zlib::Deflate.deflate(other) : other
       @stashed_value = other
       @dirty = true
     end
@@ -44,17 +48,25 @@ module ArchivedAttributes
       @backend.destroy
     end
 
-    # Returns a backend object based on the options given (e.g., filesystem, s3).
+    # Returns a backend object based on the options given (e.g., filesystem,
+    # s3).
     def instantiate_backend_from(options)
       "ArchivedAttributes::Backends::#{options[:storage].to_s.camelize}".
         constantize.new(self)
     end
 
+    def options
+      @_options
+    end
 
     private
-    def marshal?
-      @options[:marshal]
+
+    [:marshal, :gzip].each do |sym|
+      define_method("#{sym}?") do                   # def marshal?
+        options[sym].nil? ? false : options[sym]    #   options[:marshal].nil? ? false : options[:marshal]
+      end                                           # end
     end
+
   end
   
 end
--- spec/archived_attributes/archived_attribute_spec.rb
+++ spec/archived_attributes/archived_attribute_spec.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), %w[ .. spec_helper ])
 describe ArchivedAttributes::ArchivedAttribute do
   before do
     options = { :storage => 'filesystem' }
-    @aa = ArchivedAttributes::ArchivedAttribute.new(Object, Object.new, options)
+    stub_model = stub(:uuid => '43')
+    @aa = ArchivedAttributes::ArchivedAttribute.new(:glorp, stub_model, options)
   end
 
   describe "#dirty?" do
@@ -17,6 +18,30 @@ describe ArchivedAttributes::ArchivedAttribute do
     end
   end
 
+  describe "#marshal?" do
+    it "should return false if configuration option is nil" do
+      @aa.expects(:options).returns({:marshal => nil})
+      @aa.__send__(:marshal?).should be_false
+    end
+
+    it "should return value from configuration if configuration option is not nil" do
+      @aa.expects(:options).returns({:marshal => false}).times(2)
+      @aa.__send__(:marshal?).should be_false
+    end
+  end
+
+  describe "#gzip?" do
+    it "should return false if configuration option is nil" do
+      @aa.expects(:options).returns({:gzip => nil})
+      @aa.__send__(:gzip?).should be_false
+    end
+
+    it "should return value from configuration if configuration option is not nil" do
+      @aa.expects(:options).returns({:gzip => false}).times(2)
+      @aa.__send__(:gzip?).should be_false
+    end
+  end
+
   describe "#destroy" do
     it "should call destroy on the backend object" do
       backend = mock('backend', :destroy => true)
@@ -25,11 +50,13 @@ describe ArchivedAttributes::ArchivedAttribute do
     end
   end
 
-  describe "when options include :marshal" do
+  describe "when marshal is on" do
     before do
-      options = { :storage => 'filesystem', :marshal => true }
-      stub_model = stub(:uuid => '43')
-      @aa = ArchivedAttributes::ArchivedAttribute.new(Object, stub_model, options)
+      @content = 'mary had a little lamb'
+      stub_backend = stub('backend', :load => Marshal.dump(@content))
+      @aa.stubs(:options).
+        returns({ :storage => 'filesystem', :marshal => true })
+      @aa.__send__(:instance_variable_set, :'@backend', stub_backend)
     end
 
     it "should call Marshal.load to restore value" do
@@ -40,6 +67,48 @@ describe ArchivedAttributes::ArchivedAttribute do
     it "should call Marshal.dump to save value" do
       Marshal.expects(:dump)
       @aa.value = 'foo'
+    end
+
+    it "should retrieve same content string stored in Marshalled form" do
+      @aa.value.should == @content
+    end
+  end
+
+  describe "when gzip is on" do
+    before do
+      @content = "The band formed in 1988 in Oxford"
+      stub_backend = stub('backend', :load => Zlib::Deflate.deflate(@content))
+      @aa.stubs(:options).
+        returns({ :storage => 'filesystem', :gzip => true })
+      @aa.__send__(:instance_variable_set, :'@backend', stub_backend)
+    end
+
+    it "should call Zlib::Inflate.inflate to restore value" do
+      Zlib::Inflate.expects(:inflate)
+      @aa.value
+    end
+
+    it "should call Zlib::Deflate.deflate to save value" do
+      Zlib::Deflate.expects(:deflate)
+      @aa.value = 'foo'
+    end
+
+    it "should retrieve same content string stored in gzip form" do
+      @aa.value.should == @content
+    end
+  end
+
+  describe "when gzip and marshal are on" do
+    before do
+      @content = "The band formed in 1988 in Oxford"
+      stub_backend = stub('backend', :load => Zlib::Deflate.deflate(Marshal.dump(@content)))
+      @aa.stubs(:options).
+        returns({ :storage => 'filesystem', :gzip => true, :marshal => true })
+      @aa.__send__(:instance_variable_set, :'@backend', stub_backend)
+    end
+
+    it "should retrieve the same content given" do
+      @aa.value.should == @content
     end
   end
 end
